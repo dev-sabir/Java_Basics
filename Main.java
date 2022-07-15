2939e622ef381a7af11d453a001a920ddb064ee6
Mohd Sabir <sabir9202@gmail.com>
2022-07-15 16:26:38 +0530

    Types file added

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+
 public class Main{
   public static void main(String[] args){
     System.out.println("Hello World");
+    System.out.println("Welcome to Java World");
+    System.out.println(3+3);
+
   }
 }
